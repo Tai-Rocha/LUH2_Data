4045af4c92c23c166c0e02940330a917f5ba96f9
Tai-Rocha <taina013@gmail.com>
2020-10-06 16:32:04 -0300

    update

---
+++ R_scripts/LULC_script.R
@@ -0,0 +1,12 @@
+##########################################################################################
+## Generate Land-use Land-cover raster from 12 states of Land-use Harmonization Project
+## To build this raster, for each pixel the higest value among 12 states is assumed. 
+## Author script: Tainá 
+##########################################################################################
+
+lista_luh2 <- list.files("./Results/New_September/2100_test/", full.names = TRUE)
+
+all <- stack(lista_luh2)
+
+r <- which.max(all)
+writeRaster(r, "./Results/test_LULC_2100.tif")
