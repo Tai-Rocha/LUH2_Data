776f4cf3d183acefaf870edd69f4c935290dcce3
Tai-Rocha <taina013@gmail.com>
2021-02-11 17:56:06 -0300

    updating...

--- R_scripts/tests.R
+++ R_scripts/tests.R
@@ -144,3 +144,53 @@ for(j in 1:length(dir)){
   writeRaster(stack( list.files(path=dir[j], recursive=TRUE, full.names=TRUE, pattern='rain'))*2,
               paste0(dir[j],"/",strsplit(dir[j],"/")[[1]][2], "_new.tif"), overwrite=TRUE,  bylayer=TRUE )
 }
+############## Reclassificando raster
+
+#Library
+library(raster)
+library(rJava)
+library(rgeos)
+library(rgdal)
+
+## Criar uma matriz onde estarão os valores do pixel atual e o que serão substituídos
+
+matriz_reclass<-matrix(data=c(1,2,3,4,5,6,7,8,9,10,11,12,1,3,1,3,3,3,4,2,2,2,2,2),nrow=12,ncol=2)
+
+##Leitura e transformação em objeto do raster
+
+rj_raster1<-raster("./Results/Old/GLOBAL_STATES_LULC_CMIP6_850_2100/2015/2015_45/LULC_2015_CMIP6_45.tif")
+plot(rj_raster1)
+
+##Reclassificando o objeto raster
+
+raster_reclass<-reclassify(x = rj_raster1, #objeto raster
+           rcl =matriz_reclass, #matriz criada com os valores de origem e destino
+           filename="2015_reclass") #nome do arquivo de output
+
+
+##salvando o novo raster reclassificado
+writeRaster(raster_reclass,"./Results/Old/test_2015_45_reclass.tif")
+plot(raster_reclass)
+
+
+
+############# Criar uma matriz onde estarão os valores do pixel atual e o que serão substituídos
+
+matriz_reclass_2 <-matrix(data=c(1,2,3,4,5,6,7,8,9,10,11,4,2,3,1,3,3,3,3,3,3,3),nrow=11,ncol=2)
+
+##Leitura e transformação em objeto do raster
+
+raster_FAO<-raster("./data/FAO/GlcShare_v10_Dominant_10km.tif")
+plot(raster_FAO)
+
+##Reclassificando o objeto raster
+
+raster_reclass_2<-reclassify(x = raster_FAO, #objeto raster
+                           rcl =matriz_reclass_2, #matriz criada com os valores de origem e destino
+                           filename="FAO_reclass") #nome do arquivo de output
+
+plot(raster_reclass_2)
+
+##salvando o novo raster reclassificado
+writeRaster(raster_reclass_2,"./data/FAO/FAO_10km_reclass.tif")
+
