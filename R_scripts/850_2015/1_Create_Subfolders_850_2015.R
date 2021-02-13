8e181c1a5e5b826db1f0dd51c821ce3108b189df
Tai-Rocha <taina013@gmail.com>
2021-02-12 21:20:25 -0300

    extract single layers states (12 states, years from 850 to 2015)

--- R_scripts/850_2015/1_Create_Subfolders_850_2015.R
+++ R_scripts/850_2015/1_Create_Subfolders_850_2015.R
@@ -5,6 +5,7 @@
 
 #1 Create directory per year. First a read the tables with de subfolders names
 table_8502015 <- read.csv("./data/tables_to_write_in_rscripts/table_850_2015_subfolders.csv")
+
 subfolder_names <- c(table_8502015[1:1166,])
 
 #2 Loop to crate these folders in singles folder 
