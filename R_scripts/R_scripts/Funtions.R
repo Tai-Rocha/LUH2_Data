a2596a6464aba89962f759624284dc5cbb698573
Tai-Rocha <taina013@gmail.com>
2021-06-11 19:19:58 -0300

    Updated scripts. Need to create iteration with "map" instead of "for" for get_vars_t_series script

---
+++ R_scripts/R_scripts/Funtions.R
@@ -0,0 +1,116 @@
+##########################################################################
+## Funtions to extract time-series variables from netCDF file
+## And to rename files from old rasw names to bew names
+##########################################################################
+
+#Library
+
+library(sp)
+library(raster)
+library(rgdal)
+library(ncdf4)
+
+### Funtion to extract
+
+netcdf_extract <- function(x){
+
+  varr$name # Certificating that is a primnf variable
+  varr_size <- varr$varsize ## There should be 1440 lons, 720 lats, and 86 times
+  varr_dims <- varr$ndims ## 3 dimensions Long, Lat and time
+  varr_v <- varr_size[varr_dims]  ## Length of time dimension (for loop for)
+  lat <- states_45_SSP2$dim$latitude$vals  # latitude position
+  lon <- states_45_SSP2$dim$longitude$vals  # longitude position
+  listaa <- list() # primf variable list
+  
+  
+#4 Loop to get 2015 to 2100 raster of primf 
+  for (i in 1:varr_v) {
+    start <- rep(1,varr_dims)  # begin with start=(1,1,...,1)
+    start[varr_dims] <- i      # change to start=(1,1,...,i) to read timestep i
+    count <- varr_size         # begin with count=(nx,ny,...,varr_v), reads entire var
+    count[varr_dims] <- 1      # change to count=(nx,ny,...,1) to read 1 tstep
+    
+    
+    ## Seleciona variável 
+    if(varr$name == "primf")
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'primf', start = start, count = count)
+    }
+    else if(varr$name == "primn")    
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'primn', start = start, count = count)
+    }
+    else if(varr$name == "secdf")    
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'secdf', start = start, count = count)
+    }
+    else if(varr$name == "secdn")    
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'secdn', start = start, count = count)
+    }
+    else if(varr$name == "urban")    
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'urabn', start = start, count = count)
+    }
+    else if(varr$name == "c3ann")    
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'c3ann', start = start, count = count)
+    }
+    else if(varr$name == "c4ann")    
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'c4ann', start = start, count = count)
+    }
+    else if(varr$name == "c3per")    
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'c3per', start = start, count = count)
+    }
+    else if(varr$name == "c4per")    
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'c4per', start = start, count = count)
+    }
+    else if(varr$name == "c3nfx")    
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'c3nfx', start = start, count = count)
+    }
+    else if(varr$name == "pastr")    
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'pastr', start = start, count = count)
+    }
+    else if(varr$name == "range")    
+    {
+      var_var <-ncvar_get(states_45_SSP2, varid = 'range', start = start, count = count)
+    }
+    
+    #5 convert to raster
+    listaa[i] <- raster(var_var)
+  }  
+
+# Create layer stack with time dimension
+  listaa <- stack(listaa)
+  
+#7 Transpose the raster to have correct orientation
+listaa_final<-t(listaa)
+  
+listaa_final 
+# Set the coordinate reference system (CRS) and extent
+  crs(listaa_final) <- crs_object # set crs of raster stack
+  extent(listaa_final) <- c(-180,180, -90,90) # set extent
+  
+  
+# Write and save rasters in a year folders 
+  
+writeRaster(listaa_final, filename=file.path("./Results/SSP2_45_2015_2100/"), format="GTiff", bylayer=TRUE)
+
+return()
+}  
+
+
+### Funtions to rename files and than remove old files
+
+new_fnames <- function(x) 
+{file.copy(from = old_files, to = new_files)
+  file.remove(old_files)
+  }
+
+######## 
+
---
+++ R_scripts/R_scripts/get_vars_t_series.R
@@ -0,0 +1,297 @@
+########################################################################################
+# Get states
+# 4.1 prerelease R version 
+########################################################################################
+
+#Librarys
+library(sp)
+library(raster)
+library(rgdal)
+library(ncdf4)
+library(logr)
+library(rdtLite)
+
+## Initial setting 
+# Get the coordinate reference system (CRS) for raster
+epsg <- make_EPSG() # call the list of epsg
+head(epsg) # search for epsg 4326 and put below
+crs_object <-"+proj=longlat +datum=WGS84" #object of crs
+
+#3 Get the coordinate reference system (CRS) for raster
+epsg <- make_EPSG() # call the list of epsg
+head(epsg) # search for epsg 4326 and put below
+crs_object <-"+proj=longlat +datum=WGS84" #object of crs
+
+#Open NetCDF
+states_45_SSP2 <- nc_open("./data/multiple-states_input4MIPs_landState_ScenarioMIP_UofMD-MESSAGE-ssp245-2-1-f_gn_2015-2100.nc", write=TRUE, readunlim=TRUE, verbose = TRUE, auto_GMT = TRUE, suppress_dimvals = FALSE)
+
+
+########################################################################################
+# Get states
+########################################################################################
+
+#Call variables, it's is deliminted by index, i.e "[[]]". The object must call "varr" for funtions works. 
+
+# primf
+varr <- states_45_SSP2$var[[1]]  
+
+# Call function creates in script 1_funtions 
+
+primf <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_primf_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
+## primn
+
+varr <- states_45_SSP2$var[[2]]  ## Take primn variable
+
+# Call function creates in script 1_funtions 
+
+primn <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_primn_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
+## secdf
+varr <- states_45_SSP2$var[[3]]  ## Take secdf variable
+
+secdf <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_secdf_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
+
+## secdn 
+varr <- states_45_SSP2$var[[4]]  ## Take secdn variable
+
+secdn <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_secdn_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
+## urban
+varr <- states_45_SSP2$var[[5]]  ## Take urban variable
+
+urban <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_urban_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
+## c3ann
+varr <- states_45_SSP2$var[[6]]  ## Take c3ann variable
+
+c3ann <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_c3ann_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
+## c4ann
+varr <- states_45_SSP2$var[[7]]  ## Take c4ann variable
+
+c4ann <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names. You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_c4ann_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
+
+## c3per
+varr <- states_45_SSP2$var[[8]]  ## Take c3per variable
+
+c3per <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_c3per_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
+
+## c4per
+varr <- states_45_SSP2$var[[9]]  ## Take c4per variable
+
+c4per <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_c4per_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
+
+## c3nfx
+varr <- states_45_SSP2$var[[10]]  ## Take c3nfx variable
+
+c3nfx <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_c3nfx_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
+
+## pastr
+varr <- states_45_SSP2$var[[11]]  ## Take pastr variable
+
+pastr <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_pastr_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
+
+## range
+varr <- states_45_SSP2$var[[12]]  ## Take range variable
+
+range <- netcdf_extract(varr) 
+
+# List Old names
+old_files <- list.files("./Results/SSP2_45_2015_2100/", pattern = "*.tif", full.names = TRUE)
+old_files
+
+# Create new names You need change this
+new_files <- paste0("./Results/SSP2_45_2015_2100/CMIP6_Land_Use_Harmonization_range_ssp2_45_",2015:2100,".tif")   
+new_files
+
+## Call new_fnames funtion . Rename files and then remove olds files 
+new_fnames(old_files)
+
+## Clean environment and plots
+rm(list=ls()) ## list all environment objects and remove
+
+dev.off(dev.list()["RStudioGD"]) ## remove all plots
+
